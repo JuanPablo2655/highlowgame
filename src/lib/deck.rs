b08c4468c6eedb3c9e155007a1f799d9717061a4
Isidro Casiano <personal@isidrocasiano.com>
2022-03-11 01:02:40 -0500

    feat: actual deck of cards

--- src/lib/deck.rs
+++ src/lib/deck.rs
@@ -1,16 +1,47 @@
 use crate::lib::card::*;
-use rand::seq::SliceRandom;
-use rand::Rng;
 
-pub struct Deck;
+pub struct Deck {
+    cards: Vec<Card>,
+}
 
 impl Deck {
-    pub fn draw_card(&self) -> Card {
-        let random_value: u8 = rand::thread_rng().gen_range(1..14);
-        let mut rng = rand::thread_rng();
-        let suits = Suit::all();
-        let suit = suits.choose(&mut rng).unwrap();
-
-        Card::new(random_value, *suit)
+    pub fn new() -> Self {
+        let mut cards = Vec::new();
+        for value in 1..=13 {
+            for suit in Suit::all() {
+                cards.push(Card::new(value, suit))
+            }
+        }
+        Self { cards }
     }
+
+    fn from_cards(cards: &[Card]) -> Self {
+        let cards = cards.to_vec();
+        Self { cards }
+    }
+
+    pub fn draw_card(&mut self) -> Option<Card> {
+        let card = self.cards.first().cloned();
+        self.cards.remove(0);
+        card
+    }
+}
+
+#[test]
+fn test_deck_from_cards() {
+    let mut deck = Deck::from_cards(&[Card::new(1, Suit::Spades), Card::new(2, Suit::Diamonds)]);
+
+    assert_eq!(deck.draw_card().unwrap(), Some(Card::new(1, Suit::Spades)));
+    assert_eq!(
+        deck.draw_card().unwrap(),
+        Some(Card::new(2, Suit::Diamonds))
+    );
+    assert_eq!(deck.draw_card().unwrap(), None);
+}
+
+#[test]
+fn test_deck() {
+    let mut deck = Deck::new();
+    // the two same cards cannot be drawn
+    assert_ne!(deck.draw_card(), deck.draw_card());
 }
